85031c5020f17e169f9e57f12ac656c3da414c40
Yan <yans@yancomm.net>
2015-08-11 01:29:57 -0700

    add ana dep to setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 from distutils.core import setup
-setup(name='cooldict', version='1.01', py_modules=['cooldict'],)
+setup(name='cooldict', version='1.01', install_requires=['ana>=0.1'], py_modules=['cooldict'],)
