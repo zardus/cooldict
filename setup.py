93c6ea343e612f33bf9e925bb8efe8580d654412
Audrey Dutcher <audrey@rhelmot.io>
2018-08-01 17:57:13 -0700

    bump version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='cooldict',
-      version='1.02',
+      version='1.03',
       description='Some useful dict-like structures',
       url='https://github.com/zardus/cooldict',
       install_requires=['ana>=0.1'], 
